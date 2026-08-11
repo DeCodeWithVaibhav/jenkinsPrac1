2982b65b0f41053fab222a6bdd523437789de5bb
Vaibhav <Vaibhavpatidar1113@gmail.com>
2026-08-11 15:25:56 +0530

    national flag pattern

--- jenkinpractical.java
+++ jenkinpractical.java
@@ -1,14 +1,20 @@
 public class jenkinpractical {
     public static void main(String[] args) {
-        String projectName = "Jenkins Practical";
-        int buildNumber = 42;
-        double successRate = 98.6;
+        String reset = "\u001B[0m";
+        String saffron = "\u001B[38;5;208m";
+        String white = "\u001B[38;5;255m";
+        String green = "\u001B[38;5;46m";
+        String navy = "\u001B[38;5;25m";
 
-        System.out.println("========================================");
-        System.out.println("Welcome to " + projectName + "!");
-        System.out.println("Build number: " + buildNumber);
-        System.out.println("Success rate: " + successRate + "%");
-        System.out.println("Status: Build is running successfully.");
-        System.out.println("========================================");
+        System.out.println("\n" + saffron + "##################################################" + reset);
+        System.out.println(saffron + "##################################################" + reset);
+        System.out.println(saffron + "##################################################" + reset);
+        System.out.println(white + "##################################################" + reset);
+        System.out.println(white + "############## " + navy + "◉" + white + " ###############" + reset);
+        System.out.println(white + "##################################################" + reset);
+        System.out.println(green + "##################################################" + reset);
+        System.out.println(green + "##################################################" + reset);
+        System.out.println(green + "##################################################" + reset);
+        System.out.println("\n" + navy + "  Jai Hind!  " + reset + "  Indian National Flag" + "\n");
     }
 }
